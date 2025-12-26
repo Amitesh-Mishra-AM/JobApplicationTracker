1e7f71e158001b0c34f49f198ac5728477b8ceaf
Amitesh Mishra <mishraamitesh738@gmail.com>
2025-12-26 21:04:25 +0530

    feature: added pagination logic

--- backend/src/controllers/applicationController.js
+++ backend/src/controllers/applicationController.js
@@ -19,6 +19,9 @@ export const createApplication= async(req, res)=>{
 
 export const getApplications = async(req, res)=>{
     try{
+        const page= parseInt(req.query.page) ||1;
+        const limit = parseInt(req.query.limit) || 5;
+        const skip= (page-1)*limit;
          const {status , search}= req.query;
          const query={userId:req.user._id};
          if(status){
@@ -32,13 +35,16 @@ export const getApplications = async(req, res)=>{
                     {role: {$regex:search, $options:'i'}}
                     ];
         }
-        const application=await Application.find(query).sort({createdAt:-1});
-        res.status(200).json(application);
+        const total= await Application.countDocuments(query);
+        const application=await Application.find(query).sort({createdAt:-1}).skip(skip).limit(limit);
+        res.status(200).json({page, limit, total,totalPages:Math.ceil(total/limit),application});
     }catch(err){
         res.status(500).json({message: `server error : ${err}`});
     }
 }
 
+// here we can use validator in update as well but not using for learning purpose the validator code for upadte 
+// is written below this
 export const updateApplication = async(req, res)=>{
     try{
         const application= await Application.findOne({
@@ -55,6 +61,39 @@ export const updateApplication = async(req, res)=>{
         res.status(500).json({message:`server error : ${err}`});
     }
 }
+/*
+import { updateApplicationSchema } from "../validators/applicationValidator.js"; // will go to the top
+
+export const updateApplication = async (req, res) => {
+    try {
+        const parsed = updateApplicationSchema.safeParse(req.body);
+
+        if (!parsed.success) {
+            return res.status(400).json({
+                message: "Validation failed",
+                errors: parsed.error.errors
+            });
+        }
+
+        const application = await Application.findOne({
+            _id: req.params.id,
+            userId: req.user._id
+        });
+
+        if (!application) {
+            return res.status(404).json({ message: "Application not found" });
+        }
+
+        Object.assign(application, parsed.data);
+        await application.save();
+
+        res.status(200).json(application);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+*/
 
 export const deleteApplication = async(req, res)=>{
     try{
