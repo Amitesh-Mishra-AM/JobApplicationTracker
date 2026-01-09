8f5e0482492e498d0a3bf821d4508e9ed5ef4db3
Amitesh Mishra <mishraamitesh738@gmail.com>
2026-01-09 19:07:34 +0530

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,13 @@ import cors from "cors"
 const app= express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+        "https://your-frontend-name.vercel.app"
+    ],
+    credentials:true
+}));
 app.use("/api/auth", authRoutes);
 app.use("/api/applications", applicationRoutes);
 
